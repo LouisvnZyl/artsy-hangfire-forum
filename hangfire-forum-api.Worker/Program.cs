2da87306e0c3f38a3b7b50869083f6ba94f12dc3
Louis van Zyl <louis.vanzyl@entelect.co.za>
2026-08-23 21:25:42 +0200

    Use sql for redis

--- hangfire-forum-api.Worker/Program.cs
+++ hangfire-forum-api.Worker/Program.cs
@@ -41,8 +41,8 @@ builder.Services.AddHangfire(configuration =>
         .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
         .UseSimpleAssemblyNameTypeSerializer()
         .UseRecommendedSerializerSettings()
-        .UseRedisStorage(
-            builder.Configuration.GetConnectionString("Redis")!);
+        .UseSqlServerStorage(
+            builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 builder.Services.AddHangfireServer();
